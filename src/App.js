de0c6a426d0a1a2c471dab11a55c73ea7c945f87
Jackson Hagen <jackson.d.hagen@gmail.com>
2023-05-14 15:36:18 -0600

    working on implementing bootstrap for testing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import logo from './logo.svg';
+import { Button, Container, Row, Col } from 'react-bootstrap';
 import './App.css';
+import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Container>
+			<Row className="justify-content-center">
+				<Col className="">
+					<Button>haha </Button>
+				</Col>
+			</Row>
+		</Container>
+	);
 }
 
 export default App;
